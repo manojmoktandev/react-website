ae910235fe50bc6e8e5db5176ab8e705433f1996
Manoj Moktan <developermanojmoktan@gmail.com>
2024-08-28 11:11:49 -0400

    contact us page using formspree

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -35,8 +35,52 @@ const Contact = () => {
 
   return (
     <Wrapper>
-      <h1>Contact page</h1>
-    </Wrapper>
+    <h2 className="common-heading">Contact page</h2>
+
+    <iframe
+      src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=weber%20street%20+(Temu%20eccomerce)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+      width="100%"
+      height="400"
+      style={{ border: 0 }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+    <div className="container">
+      <div className="contact-form">
+        <form
+          action="https://formspree.io/f/mpwabjoe"
+          method="POST"
+          className="contact-inputs">
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            required
+            autoComplete="off"
+          />
+
+          <input
+            type="email"
+            name="Email"
+            placeholder="Email"
+            autoComplete="off"
+            required
+          />
+
+          <textarea
+            name="Message"
+            cols="30"
+            rows="10"
+            required
+            autoComplete="off"
+            placeholder="Enter you message"></textarea>
+
+          <input type="submit" value="send" />
+        </form>
+      </div>
+    </div>
+  </Wrapper>
   );
 };
 
